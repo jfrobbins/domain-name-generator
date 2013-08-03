cd53fc369f79fc3e1fcba86998e778bf068c326d
jrobb <jfrobbins@gmail.com>
2013-08-03 10:27:04 -0400

    added length arg to filter search by max word length ( ie -l 5 or --len 5) improved the word matching so it now returns words that contain a word:  ie 'pump' is in the word 'pumpkin'

--- tld.py
+++ tld.py
@@ -1,12 +1,39 @@
 from os import system as sys
 from random import choice
+import sys
 
 class DomainGetter:
   
-  def __init__(self):
+  def __init__(self, args):
     self.words = [i.strip().lower() for i in open('/usr/share/dict/words').readlines()]
     self.tlds = [i.split()[0].strip().lower() for i in open('tlds.txt').readlines()]
-  
+    words = []
+    Length = 0
+    if len(args) >= 1:
+        for i in range(0, len(args)):
+            a = args[i]
+            #catch the length arg if it exsists, and remove those args
+            if a.lower() == '-l' or a.lower() == '--len':
+                Length = float(args.pop(i + 1))
+                args.pop(i)
+                break
+
+        if Length > 0:
+            #actually filter out by lengths
+            self.words = [w for w in self.words if len(w) <= Length]
+
+        if len(args) > 0:
+            #if there are still args (non-len), do a search for the words:
+            for w in self.words:
+                for a in args:
+                    if a in w:
+                        words.append(w)
+                        break
+                        #add word to the list, and go to the next word
+            self.words = words
+#            self.words = [a for w in self.words for a in args if a in w]
+#		this list comprehension didn't work correctly...
+         
   def __iter__(self):
     while len(self.words) > 1:
       
@@ -20,7 +47,7 @@ class DomainGetter:
           yield False
 
 
-domains = DomainGetter()
+domains = DomainGetter(sys.argv[1:])
 
 for domain in domains:
   if domain:
