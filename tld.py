53bd438e2c11c55c4ac672f73bd224457a273983
jrobb <jfrobbins@gmail.com>
2013-08-05 21:32:25 -0400

    - added flag '--starts-with' to filter words by what they start with - added flag '--words-only' to return words, and ignore the tlds

--- tld.py
+++ tld.py
@@ -4,51 +4,89 @@ import sys
 
 class DomainGetter:
   
-  def __init__(self, args):
-    self.words = [i.strip().lower() for i in open('/usr/share/dict/words').readlines()]
-    self.tlds = [i.split()[0].strip().lower() for i in open('tlds.txt').readlines()]
-    words = []
-    Length = 0
-    if len(args) >= 1:
+    def __init__(self, args):
+        self.words = [i.strip().lower() for i in open('/usr/share/dict/words').readlines()]
+        self.tlds = [i.split()[0].strip().lower() for i in open('tlds.txt').readlines()]
+        words = []
+        self.length = 0
+        self.startsWith = None
+        self.wordsOnly = False
+        self.args = args
+    
+        if len(args) >= 1:
+            #there are arguments
+            args = self.getNonWordArgs()
+
+            if self.length > 0:
+                #actually filter out by lengths
+                self.words = [w for w in self.words if len(w) <= self.length]
+
+            if self.startsWith != None:
+                self.words = [w for w in self.words if w[0:len(self.startsWith)] == self.startsWith]
+
+            if len(args) > 0:
+                #if there are still args (non-len), do a search for the words:
+                for w in self.words:
+                    for a in args:
+                        if a in w:
+                            words.append(w)
+                            break
+                            #add word to the list, and go to the next word
+                self.words = words
+                #self.words = [a for w in self.words for a in args if a in w]
+                #this list comprehension didn't work correctly...
+                del words
+
+    def getNonWordArgs(self):
+        args = self.args
+        #this one pops, have to re-loop after
         for i in range(0, len(args)):
             a = args[i]
-            #catch the length arg if it exsists, and remove those args
+            #catch the length arg if it exists, and remove those args
             if a.lower() == '-l' or a.lower() == '--len':
-                Length = float(args.pop(i + 1))
+                self.length = float(args[i + 1])
+                args.pop(i + 1)
+                args.pop(i)
+                break
+        #this one pops, have to re-loop after
+        for i in range(0, len(args)):
+            a = args[i]
+            if a.lower() == '--starts-with':
+                self.startsWith = args[i + 1]
+                args.pop(i + 1)
                 args.pop(i)
                 break
 
-        if Length > 0:
-            #actually filter out by lengths
-            self.words = [w for w in self.words if len(w) <= Length]
+        for i in range(0, len(args)):
+            a = args[i]
+            if a.lower() == '--words-only':
+                self.wordsOnly = True
+                args.pop(i)
+                break
 
-        if len(args) > 0:
-            #if there are still args (non-len), do a search for the words:
-            for w in self.words:
-                for a in args:
-                    if a in w:
-                        words.append(w)
-                        break
-                        #add word to the list, and go to the next word
-            self.words = words
-#            self.words = [a for w in self.words for a in args if a in w]
-#		this list comprehension didn't work correctly...
+        self.args = args
+        return args
          
-  def __iter__(self):
-    while len(self.words) > 1:
-      
-      word = choice(self.words)
-      self.words.remove(word)
-      
-      for tld in self.tlds:
-        if word.endswith(tld):
-          yield '%s.%s' % (word.rstrip(tld), tld)
-        else:
-          yield False
+    def __iter__(self):
+        while len(self.words) > 1:
+            word = choice(self.words)
+            self.words.remove(word)
+
+            if self.wordsOnly == False:
+                for tld in self.tlds:
+                    if word.endswith(tld):
+                        yield '%s.%s' % (word.rstrip(tld), tld)
+                    else:
+                        yield False
+            else:
+                yield word    
 
 
-domains = DomainGetter(sys.argv[1:])
+###############################################
 
-for domain in domains:
-  if domain:
-    print domain
+if __name__ == '__main__':
+    domains = DomainGetter(sys.argv[1:])
+
+    for domain in domains:
+      if domain:
+        print domain
